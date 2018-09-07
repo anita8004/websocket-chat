ef5f6703cedcf9a02b6329401fb01ee1dfdd737a
Yunhsiu Li <anita8004@gmail.com>
2018-09-07 17:21:56 +0800

    改網址

--- socket/index.js
+++ socket/index.js
@@ -1,12 +1,13 @@
 const Messages = require('../models/Messages.js')
 const moment = require('moment')
+const uri = process.env.MONGODB_ADDON_URI || 'mongodb://u9jm2cn1hskxsp0:LFg1Hwzr6nDvqlSfQP4H@b3b9aoeakowtlea-mongodb.services.clever-cloud.com:27017/b3b9aoeakowtlea'
 
 class SocketHander{
   constructor() {
     this.db;
   }
   connect() {
-    this.db = require('mongoose').connect('mongodb://u9jm2cn1hskxsp0:LFg1Hwzr6nDvqlSfQP4H@b3b9aoeakowtlea-mongodb.services.clever-cloud.com:27017/b3b9aoeakowtlea')
+    this.db = require('mongoose').connect(uri)
     this.db.Promise = global.Promise
   }
   getMessages() {
